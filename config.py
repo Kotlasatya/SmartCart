5777ea554b031e704d28c4b7dd3aa29df833d870
Kotlasatya <satyakotla398@gmail.com>
2026-02-13 12:35:13 +0530

    SmartCart app

---
+++ config.py
@@ -0,0 +1,24 @@
+SECRET_KEY = "satyakotla5566"   # used for sessions
+
+# MySQL Database Configuration
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "Satyakotla@123"
+DB_NAME = "smartcart_db"
+
+
+# Email SMTP Settings
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'satyakotla398@gmail.com'
+MAIL_PASSWORD = 'fkdi ftxm lcsj ykdh'
+
+
+RAZORPAY_KEY_ID = "rzp_test_SFDifTMVu60b5H"
+RAZORPAY_KEY_SECRET = "w95Y6U9okTH8Fqo0DQRFlARU"
+
+
+
+
+
