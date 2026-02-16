1c11860721f9d742b100fbdd7bc99dd26978ba97
kotla satya vardhan reddy <94086388+Kotlasatya@users.noreply.github.com>
2026-02-16 17:54:06 +0530

    Update app.pynew

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ def inject_cart_count():
 app.config['MAIL_SERVER'] = config.MAIL_SERVER
 app.config['MAIL_PORT'] = config.MAIL_PORT
 app.config['MAIL_USE_TLS'] = config.MAIL_USE_TLS
+app.config['MAIL_USE_SSL'] = config.MAIL_USE_SSL
 app.config['MAIL_USERNAME'] = config.MAIL_USERNAME
 app.config['MAIL_PASSWORD'] = config.MAIL_PASSWORD
 
