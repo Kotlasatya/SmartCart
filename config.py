097b0612a23f7be2a2a4be6c2cf4688cd0c00eb3
Kotlasatya <satyakotla398@gmail.com>
2026-02-16 19:42:31 +0530

    Switch SMTP to SSL port 465 for better reliability

--- config.py
+++ config.py
@@ -9,17 +9,12 @@ DB_NAME = "smartcart_db"
 
 # Email SMTP Settings
 MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USE_SSL = False
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
 MAIL_USERNAME = 'satyakotla398@gmail.com'
 MAIL_PASSWORD = 'fkdi ftxm lcsj ykdh'
 
 
 RAZORPAY_KEY_ID = "rzp_test_SFDifTMVu60b5H"
 RAZORPAY_KEY_SECRET = "w95Y6U9okTH8Fqo0DQRFlARU"
-
-
-
-
-
